96a5dd9b34983eb4910e336cba9fab60a368d73f
devrepo <dev_now@yahoo.com>
2020-06-02 23:21:18 +0530

    removed unused and commented code

--- src/sw-template.js
+++ src/sw-template.js
@@ -28,28 +28,6 @@ if ('function' === typeof importScripts) {
             blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
         });
         
-   
-        /*workbox.routing.registerRoute(
-            /\.(?:png|gif|jpg|jpeg)$/,
-            new workbox.strategies.CacheFirst({
-            cacheName: 'images',
-            plugins: [
-                new workbox.expiration.Plugin({
-                maxEntries: 60,
-                maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-                }),
-            ],
-            })
-        );*/
-
-        const matchCb = ({url, request, event}) => {
-            //var patt = new RegExp("^https:\/\/hn\.algolia\.com\/api\/v1\/(search?(.*)?)?");
-            //var res = patt.test(url.pathname);
-            var res = url.pathname == "https://hn.algolia.com/api/v1/search?tags=story&hitsPerPage=30&page=0"
-            console.log("URL matches");
-            return res
-        };
-
         workbox.routing.registerRoute(new RegExp('^https:\/\/hn\.algolia\.com\/api\/v1\/(search?(.*)?)?'),
             new workbox.strategies.NetworkFirst({
                 cacheName: 'api-cache',
